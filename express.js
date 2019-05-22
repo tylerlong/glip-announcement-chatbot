94b60dc97d47179159ec67e401d7a13625d2679e
Tyler Long <tyler4long@gmail.com>
2019-05-22 15:55:28 -0700

    Create app.js file

--- express.js
+++ express.js
@@ -1,10 +1,2 @@
-const createApp = require('ringcentral-chatbot/dist/apps').default
-
-const handle = async event => {
-  const { type, text, group, bot } = event
-  if (type === 'Message4Bot' && text === 'ping') {
-    await bot.sendMessage(group.id, { text: 'pong' })
-  }
-}
-const app = createApp(handle)
+const app = require('./app')
 app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT)
