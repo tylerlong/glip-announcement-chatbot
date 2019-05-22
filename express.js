d092750414db4a2871022778424f9dc82a084ef8
Tyler Long <tyler4long@gmail.com>
2019-05-21 23:20:37 -0700

    Setup the bot

---
+++ express.js
@@ -0,0 +1,10 @@
+const createApp = require('ringcentral-chatbot/dist/apps').default
+
+const handle = async event => {
+  const { type, text, group, bot } = event
+  if (type === 'Message4Bot' && text === 'ping') {
+    await bot.sendMessage(group.id, { text: 'pong' })
+  }
+}
+const app = createApp(handle)
+app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT)
