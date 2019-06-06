64fd7b10451add68f353c92bb9968da40d8a4816
Tyler Long <tyler4long@gmail.com>
2019-06-06 15:30:32 -0700

    Fix some typos

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const handle = async event => {
     if (text === 'ping') {
       await bot.sendMessage(group.id, { text: 'pong' })
     } else if (text === 'about' || text === 'help' || text === '') {
-      await bot.sendMessage(group.id, { text: 'Mention me and start your message with "ANNOUNCEMENT:" and I will mail the message to all the team members.' })
+      await bot.sendMessage(group.id, { text: 'Mention me and start your message with "ANNOUNCEMENT:" and I will email the message to all the team members.' })
     } else if (text.startsWith('ANNOUNCEMENT:')) {
       const persons = await bot.rc.batchGet('/restapi/v1.0/glip/persons', group.members, 30)
       const emails = persons
@@ -16,13 +16,13 @@ const handle = async event => {
         .filter(email => !email.endsWith('.bot.glip.net'))
       await bot.sendMessage(group.id, { text: 'Sending announcement above via email to ' + emails.join(', ') })
       gmailSend({
-        user: process.env.GMAIL_ADDRESS, // Your GMail account used to send emails
-        pass: process.env.GMAIL_PASSWORD, // Application-specific password
+        user: process.env.GMAIL_ADDRESS,
+        pass: process.env.GMAIL_PASSWORD,
         to: emails, // Send to yourself
         subject: 'ANNOUNCEMENT',
         text: text.substring(13).trim()
       })({})
-      await delay(3000) // want for email sending
+      await delay(3000) // wait for email sending
     } else {
       // message is not for the bot, do nothing.
     }
