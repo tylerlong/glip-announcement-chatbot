a9ed4ed7e0f3cf219db805e8ccc11e89457d1822
Tyler Long <tyler4long@gmail.com>
2019-06-06 13:58:58 -0700

    Try sendmail

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const createApp = require('ringcentral-chatbot/dist/apps').default
+const sendmail = require('sendmail')()
 
 const handle = async event => {
   const { type, text, group, bot } = event
@@ -9,11 +10,23 @@ const handle = async event => {
       await bot.sendMessage(group.id, { text: 'Mention me and start your message with "ANNOUNCEMENT:" and I will mail the message to all the team members.' })
     } else if (text.startsWith('ANNOUNCEMENT:')) {
       const persons = await bot.rc.batchGet('/restapi/v1.0/glip/persons', group.members, 30)
-      await bot.sendMessage(group.id, { text: 'Sending announcement email to ' + persons
+      sendmail({
+        from: 'announment-bot@ringcentral.com',
+        to: persons
+          .map(person => person.email)
+          .filter(email => !email.endsWith('.bot.glip.net'))
+          .join(', '),
+        subject: 'ANNOUNCEMENT',
+        html: text.substring(13).trim()
+      }, function (err, reply) {
+        console.log(err && err.stack)
+        console.dir(reply)
+      })
+      await bot.sendMessage(group.id, { text: 'Sending announcement above via email to ' + persons
         .map(person => person.email)
         .filter(email => !email.endsWith('.bot.glip.net'))
-        .join(', ') + '. Email body is: ' + text }
-      )
+        .join(', ')
+      })
     } else {
       // message is not for the bot, do nothing.
     }
