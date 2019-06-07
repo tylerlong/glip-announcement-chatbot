5e05d22409537851d25b520dcd37f6b421fc7270
Tyler Long <tyler4long@gmail.com>
2019-06-06 18:04:42 -0700

    Auto maintain every day

--- lambda.js
+++ lambda.js
@@ -1,6 +1,11 @@
 const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
 const serverlessHTTP = require('serverless-http')
 const app = require('./app')
+const axios = require('axios')
 
 module.exports.app = serverlessHTTP(app)
 module.exports.proxy = createAsyncProxy('app')
+module.exports.maintain = async () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`, undefined, { auth: {
+  username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
+  password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD
+} })
