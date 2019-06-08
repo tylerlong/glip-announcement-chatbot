af8c79e9420b3e90894e5755ea32b414ac54ee51
Tyler Long <tyler4long@gmail.com>
2019-06-08 10:08:34 -0700

    Express app auto maintain

--- express.js
+++ express.js
@@ -1,6 +1,12 @@
+const axios = require('axios')
 const app = require('./app')
 const sendEmail = require('./crontab')
 
 app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT)
 
-setInterval(() => sendEmail(), 60000)
+setInterval(() => sendEmail(), 60 * 1000)
+setInterval(async () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`, undefined, { auth: {
+  username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
+  password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD
+} })
+, 24 * 60 * 60 * 1000)
