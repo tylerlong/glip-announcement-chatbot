b0272d904750055e5ea1aa7a78c764f5d9df6c85
Tyler Long <tyler4long@gmail.com>
2019-07-05 15:20:43 -0700

    No proxy for GET

--- lambda.js
+++ lambda.js
@@ -1,10 +1,12 @@
 const serverlessHTTP = require('serverless-http')
 const axios = require('axios')
+const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
 
 const app = require('./app')
 const sendEmail = require('./crontab')
 
 module.exports.app = serverlessHTTP(app)
+module.exports.proxy = createAsyncProxy('app')
 module.exports.maintain = async () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`, undefined, { auth: {
   username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
   password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD
